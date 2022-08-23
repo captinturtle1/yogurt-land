4885026cf74971a90a51b92784bfb6c511ef30e0
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-08-23 16:49:32 -0700

    initial commit

---
+++ components/addresses.js
@@ -0,0 +1,12 @@
+export const addressesNotLowercase= 
+[
+    "",
+    "",
+    "",
+];
+
+const lower = addressesNotLowercase.map(element => {
+    return element.toLowerCase();
+});
+
+export const addresses = lower;
