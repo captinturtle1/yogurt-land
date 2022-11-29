657c2b4ab889f1cb01a51531323a80418b4f5f4d
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-11-29 01:50:42 -0800

    page hero
    Changed out picture. Removed Gurts title

--- components/Landing.js
+++ components/Landing.js
@@ -1,9 +1,7 @@
 export default function Landing() {
     return (
-      <div id="Landing" className="flex min-h-[500px]">
-        <img src="bglayer1.png" className="z-[-1] absolute w-screen min-h-[1000px] object-cover"></img>
-        <img src="bglayer.png" className="z-[1] absolute w-screen min-h-[1000px] object-cover"></img>
-        <div className="mx-auto mt-[200px] lg:mt-[12%] mb-[7%] font-bold text-[20vw] drop-shadow-lg text-white">Gurts</div>
+      <div id="Landing" className="flex min-h-[800px]">
+        <img src="bglayer1.jpg" className="z-[-1] absolute w-screen min-h-[1000px] object-cover"></img>
       </div>
     )
   }
