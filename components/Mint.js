640c52017eb260a0a92a52f8421a87da55833330
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-11-29 02:01:02 -0800

    branch init

--- components/Mint.js
+++ components/Mint.js
@@ -1,125 +1,6 @@
-import { useState, useEffect} from 'react';
-
 import Wave from '../public/wave.svg';
-import {
-  getGurtsTotalSupply,
-  getGurtsMaxSupply,
-  getGurtsPrice,
-  getGurtsPrivateSale,
-  getGurtsPublicSale,
-  getGurtsPassSale,
-  getHasMintedPublic,
-  getHasMintedWhitelist,
-  getIsWhitelisted
-} from './etherscomponents/contractFunctions';
 
 export default function Navbar() {
-  const [connectedWallet, setConnectedWallet] = useState("");
-
-  const [privateSale, setPrivateSale] = useState(false);
-  const [publicSale, setPublicSale] = useState(false);
-  const [claimSale, setClaimSale] = useState(false);
-
-  const [maxSupply, setMaxSupply] = useState(0);
-  const [totalSupply, setTotalSupply] = useState(0);
-  const [price, setPrice] = useState(0);
-
-  const [hasMintedPublic, setHasMintedPublic] = useState(false);
-  const [hasMintedWhitelist, setHasMintedWhitelist] = useState(false);
-
-  const [isWhitelisted, setIsWhitelisted] = useState(false);
-
-  useEffect(() => {
-    // checks if ethereum provider is detected
-    try{
-        let provider = new ethers.providers.Web3Provider(window.ethereum);
-        if (typeof provider !== 'undefined') {
-            window.ethereum.on('accountsChanged', (accounts) => {
-            console.log(`account changed to ${accounts[0]}`);
-            setConnectedWallet(accounts[0]);
-        })
-
-        window.ethereum.on('chainChanged', (chainId) => {
-            console.log(`network changed to ${chainId}. Reloading...`);
-            window.location.reload();
-        })
-
-        window.ethereum.on('disconnect', (providerRpcError) => {
-            console.log('ethereum disconnected', providerRpcError);
-            setConnectedWallet("");
-        })
-
-        getAccount();
-      }
-    } catch (err) {
-        console.log(err);
-        console.log("No ethereum service.");
-    }
-  },[connectedWallet]);
-
-  useEffect(() => {
-    getGurtsTotalSupply().then(response => {
-      console.log("Total Supply: ", response);
-      setTotalSupply(response);
-    }).catch(console.log);
-
-    getGurtsMaxSupply().then(response => {
-      console.log("Max Supply: ", response);
-      setMaxSupply(response);
-    }).catch(console.log);
-
-    getGurtsPrice().then(response => {
-      console.log("Gurts Price: ", response);
-      setPrice(response);
-    }).catch(console.log);
-
-    getGurtsPrivateSale().then(response => {
-      console.log("Private Sale: ", response);
-      setPrivateSale(response);
-    }).catch(console.log);
-
-    getGurtsPublicSale().then(response => {
-      console.log("Public Sale: ", response);
-      setPublicSale(response);
-    }).catch(console.log);
-
-    getGurtsPassSale().then(response => {
-      console.log("Pass Sale: ", response);
-      setClaimSale(response);
-    }).catch(console.log);
-  }, [])
-
-  const requestAccount = async () => {
-    try {
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const getAccount = async () => {
-  try {
-      console.log('ethereum connected');
-      let accounts = await window.ethereum.request({ method: "eth_accounts" });
-      console.log(`${accounts[0]} connected`);
-      setConnectedWallet(accounts[0]);
-
-      getHasMintedPublic(accounts[0]).then(response => {
-        setHasMintedPublic(response);
-      }).catch(console.log);
-
-      getHasMintedWhitelist(accounts[0]).then(response => {
-        setHasMintedWhitelist(response);
-      }).catch(console.log);
-
-      getIsWhitelisted(accounts[0]).then(response => {
-        setIsWhitelisted(response);
-      }).catch(console.log);
-  } catch (err) {
-      console.log(err);
-  }
-}
-
   return (
     <div id="Mint" className="relative h-[1100px] grid grid-cols-1 lg:grid-cols-3">
       <svg className="absolute w-screen h-[1080px] fill-pink-200 z-[1] drop-shadow-[0px_-10px_10px_rgba(0,0,0,0.25)]"><Wave/></svg>
@@ -129,7 +10,7 @@ const getAccount = async () => {
       <div className="col-span-2 flex z-[2]">
         <div className="bg-white grow h-[600px] my-0 lg:my-auto mx-10 lg:mx-40 rounded-3xl drop-shadow-xl flex flex-col">
           <img src="strawberry.png" className="absolute w-16 bottom-16 right-16 animate-wiggle"/>
-          <div className="text-black mx-auto font-bold text-5xl m-auto animate-bounce">Mint coming soon!</div>
+          <div className="text-black mx-auto font-bold text-2xl xl:text-5xl m-auto animate-bounce">Mint coming soon!</div>
         </div>
       </div>
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
     <div className="z-[10] fixed top-0 w-screen flex h-16 bg-white text-black select-none">
       <ScrollLink to="Landing" spy={true} smooth={true} offset={0} duration={600} className="flex absolute h-16 ">
           <img src="YV_LOGO2.png" className="h-10 my-auto ml-16 cursor-pointer"/>
-          <div className="font-bold my-auto text-2xl cursor-pointer">Yogurt Verse</div>
+          <div className="font-bold my-auto text-2xl cursor-pointer">Gurts</div>
       </ScrollLink>
       <div className="m-auto hidden lg:flex gap-5 font-bold">
           <ScrollLink to="Mint" spy={true} smooth={true} offset={0} duration={600} className="text-black hover:text-zinc-500 active:text-zinc-600 transition-all cursor-pointer">mint</ScrollLink>
@@ -19,11 +19,11 @@ export default function Navbar() {
           <ScrollLink to="Faq" spy={true} smooth={true} offset={0} duration={600} className="text-black hover:text-zinc-500 active:text-zinc-600 transition-all cursor-pointer">faq</ScrollLink>
       </div>
       <div className="absolute right-0 h-16 hidden lg:flex mr-16">
-        <Link href="/stake">
-          <div className="m-auto bg-[#765050] hover:bg-[#8f6464] active:bg-[#5e3f3f] px-5 py-2 text-white rounded-full font-bold transition-all cursor-pointer">
+
+          <div className="m-auto bg-[#5e3f3f] px-5 py-2 text-gray-200 rounded-full font-bold cursor-wait">
             <a>Stake</a>
           </div>
-        </Link>
+
       </div>
       <div className=" flex lg:hidden">
           <AiOutlineMenu onClick={() => setMobileMenu(!mobileMenu)} className={mobileMenu ? "absolute right-0 h-16 mr-16 cursor-pointer text-2xl invisible opacity-0 transition-all" : "absolute right-0 h-16 mr-16 cursor-pointer text-2xl visible opacity-100 transition-all"}/>
@@ -35,11 +35,11 @@ export default function Navbar() {
         <ScrollLink onClick={() => setMobileMenu(false)} to="Mint" spy={true} smooth={true} offset={0} duration={600} className="text-white hover:text-zinc-300 active:text-zinc-400 transition-all cursor-pointer">mint</ScrollLink>
         <ScrollLink onClick={() => setMobileMenu(false)} to="About" spy={true} smooth={true} offset={0} duration={600} className="text-white hover:text-zinc-300 active:text-zinc-400 transition-all cursor-pointer">about</ScrollLink>
         <ScrollLink onClick={() => setMobileMenu(false)} to="Faq" spy={true} smooth={true} offset={0} duration={600} className="text-white hover:text-zinc-300 active:text-zinc-400 transition-all cursor-pointer">faq</ScrollLink>
-        <Link href="/stake">
-          <div className="mt-10 bg-zinc-800 hover:bg-zinc-900 active:bg-zinc-700 px-5 py-2 text-white rounded-full font-bold transition-all cursor-pointer">
+
+          <div className="mt-10 bg-zinc-800 px-5 py-2 text-gray-900 rounded-full font-bold cursor-wait">
             <a>Stake</a>
           </div>
-        </Link>
+
       </div>
     </div>
     </div>
