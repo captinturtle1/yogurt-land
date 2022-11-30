efe89a6b5f7deb12a51434f99c0d3bb040361cd3
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-11-30 06:40:31 -0800

    Update Mint.js
    pass claim has tokenId form to mint from pass Id's

--- components/Mint.js
+++ components/Mint.js
@@ -36,6 +36,9 @@ export default function Navbar() {
   const [ygBalance, setYGBalance] = useState(0);
 
   const [mintTab, setMintTab] = useState(0);
+  
+  const [tokenIds, setTokensIds] = useState("");
+  const [validInput, setValidInput] = useState(false);
 
   useEffect(() => {
     // checks if ethereum provider is detected
@@ -104,105 +107,125 @@ export default function Navbar() {
     } catch (err) {
         console.log(err);
     }
-}
-
-const getAccount = async () => {
-  try {
-      console.log('ethereum connected');
-      let accounts = await window.ethereum.request({ method: "eth_accounts" });
-      
-      if (accounts[0]) {
-        console.log(`${accounts[0]} connected`);
-        setConnectedWallet(accounts[0]);
-
-        getHasMintedPublic(accounts[0]).then(response => {
-          setHasMintedPublic(response);
-          console.log("Minted Public: ",  response);
-        }).catch(console.log);
-
-        getHasMintedWhitelist(accounts[0]).then(response => {
-          setHasMintedWhitelist(response);
-          console.log("Minted Whitelist: ",  response);
-        }).catch(console.log);
-
-        getIsWhitelisted(accounts[0]).then(response => {
-          setIsWhitelisted(response);
-          console.log("Is Whitelisted: ",  response);
-        }).catch(console.log);
-
-        getYGBalance(accounts[0]).then(response => {
-          setYGBalance(response);
-          console.log("YG Balance: ",  response);
-        }).catch(console.log);
-      } else {
-        console.log("no account connected");
-        setConnectedWallet("");
-      }
-  } catch (err) {
-      console.log(err);
   }
-}
 
-const handleMintPublic = () => {
-  mintPublic().then(response => {
-    console.log(response);
-  }).catch(console.log);
-}
+  const getAccount = async () => {
+    try {
+        console.log('ethereum connected');
+        let accounts = await window.ethereum.request({ method: "eth_accounts" });
 
-const handleMintWhitelist = () => {
-  mintWhitelist().then(response => {
-    console.log(response);
-  }).catch(console.log);
-}
+        if (accounts[0]) {
+          console.log(`${accounts[0]} connected`);
+          setConnectedWallet(accounts[0]);
 
-const handleMintWithPass = () => {
-  let tokenIds = [];
-  mintWithPass(tokenIds).then(response => {
-    console.log(response);
-  }).catch(console.log);
-}
+          getHasMintedPublic(accounts[0]).then(response => {
+            setHasMintedPublic(response);
+            console.log("Minted Public: ",  response);
+          }).catch(console.log);
+
+          getHasMintedWhitelist(accounts[0]).then(response => {
+            setHasMintedWhitelist(response);
+            console.log("Minted Whitelist: ",  response);
+          }).catch(console.log);
+
+          getIsWhitelisted(accounts[0]).then(response => {
+            setIsWhitelisted(response);
+            console.log("Is Whitelisted: ",  response);
+          }).catch(console.log);
+
+          getYGBalance(accounts[0]).then(response => {
+            setYGBalance(response);
+            console.log("YG Balance: ",  response);
+          }).catch(console.log);
+        } else {
+          console.log("no account connected");
+          setConnectedWallet("");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+  const handleMintPublic = () => {
+    mintPublic().then(response => {
+      console.log(response);
+    }).catch(console.log);
+  }
+
+  const handleMintWhitelist = () => {
+    mintWhitelist().then(response => {
+      console.log(response);
+    }).catch(console.log);
+  }
+
+  const handleMintWithPass = () => {
+    if (/^[0-9]+(,[0-9]+)*$/.test(tokenIds)) {
+      let stringArray = tokenIds.split(",");
+      let numberArray = stringArray.map(element => parseInt(element))
+      console.log(numberArray);
+      mintWithPass(numberArray).then(response => {
+        console.log(response);
+      }).catch(console.log);
+    }
+  }
+
+  const handleTokenIdInput = (e) => {
+    setTokensIds(e.target.value);
+    if (/^[0-9]+(,[0-9]+)*$/.test(e.target.value)) {
+      setValidInput(true);
+    } else {
+      setValidInput(false);
+    }
+    
+  }
 
   return (
-    <div id="Mint" className="relative h-[1100px] grid grid-cols-1 lg:grid-cols-3">
+    <div id="Mint" className="relative h-[1100px] grid grid-cols-1 xl:grid-cols-3">
       <svg className="absolute w-screen h-[1080px] fill-pink-200 z-[1] drop-shadow-[0px_-10px_10px_rgba(0,0,0,0.25)]"><Wave/></svg>
       <div className="relative col-span-1 z-[2]">
-          <img src="bgyogurtformint2.png" className="absolute bottom-10 left-16 w-[600px] hidden lg:block"/>
+          <img src="bgyogurtformint2.png" className="absolute bottom-10 left-16 w-[600px] hidden xl:block"/>
       </div>
       <div className="col-span-2 flex z-[2]">
-        <div className="bg-white grow h-[600px] my-0 lg:my-auto mx-10 lg:mx-40 rounded-3xl drop-shadow-xl flex flex-col">
+        <div className="bg-white grow h-[600px] my-0 xl:my-auto mx-10 lg:mx-40 rounded-3xl drop-shadow-xl flex flex-col">
           <img src="strawberry.png" className="absolute w-16 bottom-16 right-16 animate-wiggle"/>
           <div onClick={requestAccount} className={connectedWallet == "" ?
               "text-white m-auto flex font-bold bg-slate-700 hover:bg-slate-800 h-20 w-64 rounded-xl transition-all opacity-100 visible cursor-pointer select-none" :
-              "text-white m-auto flex font-bold h-[500px] w-[800px] rounded-xl transition-all"
+              "text-white m-auto flex font-bold h-[80%] w-[80%] rounded-xl transition-all"
           }>
             {connectedWallet == "" ? (
               <div className="m-auto text-5xl">Connect</div>
             ):(
               <div className="mx-auto text-black flex flex-col">
-                <div className="mx-auto flex text-xl gap-5 mb-32">
+                <div className="mx-auto flex text-sm lg:text-xl gap-5 mb-32">
 
                   <div onClick={() => setMintTab(0)} className={mintTab == 0 ?
-                    "bg-green-400 text-white py-2 px-5 rounded-xl select-none transition-all" :
-                    "bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded-xl cursor-pointer select-none transition-all"
+                    "bg-green-400 text-white py-2 px-2 lg:px-5 rounded-xl select-none transition-all" :
+                    "bg-gray-100 hover:bg-gray-200 py-2 px-2 lg:px-5 rounded-xl cursor-pointer select-none transition-all"
                   }>Public</div>
 
-                    <div onClick={() => setMintTab(1)} className={mintTab == 1 ?
-                      "bg-green-400 text-white py-2 px-5 rounded-xl select-none transition-all" :
-                      "bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded-xl cursor-pointer select-none transition-all"
-                    }>Whitelist</div>
+                  <div onClick={() => setMintTab(1)} className={mintTab == 1 ?
+                    "bg-green-400 text-white py-2 px-2 lg:px-5 rounded-xl select-none transition-all" :
+                    "bg-gray-100 hover:bg-gray-200 py-2 px-2 lg:px-5 rounded-xl cursor-pointer select-none transition-all"
+                  }>Whitelist</div>
 
                   <div onClick={() => setMintTab(2)} className={mintTab == 2 ?
-                    "bg-green-400 text-white py-2 px-5 rounded-xl select-none transition-all" :
-                    "bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded-xl cursor-pointer select-none transition-all"
+                    "bg-green-400 text-white py-2 px-2 lg:px-5 rounded-xl select-none transition-all" :
+                    "bg-gray-100 hover:bg-gray-200 py-2 px-2 lg:px-5 rounded-xl cursor-pointer select-none transition-all"
                   }>Pass Claim</div>
 
                 </div>
                 <div className="mx-auto">Minted: </div>
-                <div className="mx-auto w-[500px] h-[30px] relative">
+                <div className="mx-auto w-[500px] h-[30px] relative hidden 2xl:block">
                   <div className="bg-green-700 absolute flex rounded-xl" style={{width: 500, height: 30}}>
                     <div className="m-auto text-white z-[1]">{totalSupply}/{maxSupply}</div>
                   </div>
                   <div className="bg-green-500 absolute rounded-xl" style={{width: 500 * ((totalSupply < 180 ? 180 : totalSupply) / maxSupply), height: 30}}></div>
+                </div>
+                <div className="mx-auto w-[300px] h-[30px] relative block 2xl:hidden">
+                  <div className="bg-green-700 absolute flex rounded-xl" style={{width: 300, height: 30}}>
+                    <div className="m-auto text-white z-[1]">{totalSupply}/{maxSupply}</div>
+                  </div>
+                  <div className="bg-green-500 absolute rounded-xl" style={{width: 300 * ((totalSupply < 180 ? 180 : totalSupply) / maxSupply), height: 30}}></div>
                 </div>
                 {mintTab == 0 ? (
                   <div className="animate-introSlide flex flex-col">
@@ -257,7 +280,24 @@ const handleMintWithPass = () => {
                     {totalSupply < maxSupply ? (
                       <>
                         {claimSale ? (
-                          <div onClick={handleMintPublic} className="mx-auto mt-8 bg-green-500 hover:bg-green-400 px-5 py-2 rounded-xl text-white cursor-pointer select-none transition-all">Claim</div>
+                          <>
+                            <form className="flex flex-col mx-auto mt-4">
+                              <div>Token Id(s)</div>
+                              <input
+                                value={tokenIds}
+                                type="text"
+                                pattern="^[0-9]+(,[0-9]+)*$"
+                                onChange={handleTokenIdInput}
+                                placeholder="1, 2, 3, 4"
+                                className="w-96 pl-2 p-1 focus:outline-none rounded-lg bg-neutral-700 invalid:ring invalid:ring-red-300 invalid:text-red-400 text-sm text-white"
+                              />
+                            </form>
+                            {validInput ? (
+                              <div onClick={handleMintWithPass} className="mx-auto mt-8 bg-green-500 hover:bg-green-400 px-5 py-2 rounded-xl text-white cursor-pointer select-none transition-all">Claim</div>
+                            ):(
+                              <div className="mx-auto mt-8 bg-zinc-500 px-5 py-2 rounded-xl text-white select-none transition-all">Claim</div>
+                            )}
+                          </>
                         ):(
                           <div className="mx-auto mt-8 bg-zinc-500 px-5 py-2 rounded-xl text-white select-none transition-all">Claim Not Live</div>
                         )}
