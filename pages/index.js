fd6cefe97b7d02f10a0e3cbc73a1aac6045c33d5
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-11-30 22:16:48 -0800

    overflow fix, faq page
    added content to faq dropdowns
    
    fixed weird overflow issue

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-hidden">
       <Head>
         <title>Gurts</title>
         <meta name="description" content="Gurts webpage"/>
